23a364f327dacca1bde7df32d2d0c143ecb05870
rennspigelmyer <catherinespigelmyer95@gmail.com>
2021-09-22 09:55:53 -0400

    Variables Exercise completed with help from review

---
+++ script.js
@@ -0,0 +1,44 @@
+// 1
+// You can't change the value when you're using the 'const' variable.
+
+//2
+let num = 20;
+
+//3 
+num = 15;
+
+//4 
+num += 7;
+
+//5 
+num--;
+
+console.log(num)
+
+
+//6
+/*var num = 20;
+//num = "This is not a number";
+var num = "This-is-not-a-number";
+//console.log(This-is-not-a-number)*/
+/*console.log(num)*/
+// Above is my attempt. Below is the correct answer.
+num = "This is not a number";
+
+//7 & 8
+// let undefinedBlank; 
+// console.log(undefinedBlank)
+/* let blank = undefined
+console.log(blank) */
+// Above is my attempt. Below is the answer.
+let blank;
+
+//8 
+console.log(blank);
+
+//9
+blank = null;
+console.log(blank);
+
+
+
